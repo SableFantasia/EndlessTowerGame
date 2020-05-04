e0fe821333e9d194a001a52ff78f8d2e883b2e3c
VarXel <darkbionicle@live.it>
2020-05-04 20:28:15 +0200

    Player can shoot and move now

---
+++ Riven_GameplayPrototype/Assets/Scripts/PlayerMechanics/Bullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    private float bulletLifeTime;
+
+    public void Update()
+    {
+        bulletLifeTime += Time.deltaTime;
+
+        if(bulletLifeTime >= 2 && gameObject != null)
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+
+        Debug.Log(collision.gameObject.name);
+    }
+}
